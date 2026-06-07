2409acf67808d79dde2834695f87f7acf79ea0ba
Kevin Stange <kstange@gmail.com>
2026-06-07 16:03:22 -0500

    Update copyrights

--- QuickSplit.lua
+++ QuickSplit.lua
@@ -1,7 +1,7 @@
 --
 -- QuickSplit
 --
--- Copyright 2025 SimGuy
+-- Copyright 2025 - 2026 SimGuy
 --
 -- Use of this source code is governed by an MIT-style
 -- license that can be found in the LICENSE file or at
